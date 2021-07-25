ca22ff1c73edf6a751b9ba03d1159285f1bb8113
Preet Shah <shahpreetk@gmail.com>
2021-07-25 17:42:47 +0530

    fixed navbar active page css

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,11 @@
+import { NavLink } from "react-router-dom";
 const navigation = [
-  { name: 'Profile', href: '/profile', current: true },
-  { name: 'Birthday Twin', href: '/my-birthday-twin', current: false },
+  { name: "Profile", href: "/profile" },
+  { name: "Birthday Twin", href: "/my-birthday-twin" },
 ];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 const NavigationBar = () => {
@@ -15,17 +16,20 @@ const NavigationBar = () => {
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <div className="flex space-x-4">
               {navigation.map((item) => (
-                <a
+                <NavLink
                   key={item.name}
-                  href={item.href}
+                  to={item.href}
+                  activeClassName="selectedNavbarOption"
                   className={classNames(
-                    item.current ? 'bg-rose-800 text-white' : 'text-white hover:bg-rose-200 hover:text-rose-800',
-                    'px-3 py-2 rounded-md text-md font-medium mr-5'
+                    item.current
+                      ? "bg-rose-800 text-white"
+                      : "text-white hover:bg-rose-200 hover:text-rose-800",
+                    "px-3 py-2 rounded-md text-md font-medium mr-5"
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
-                </a>
+                </NavLink>
               ))}
             </div>
           </nav>
